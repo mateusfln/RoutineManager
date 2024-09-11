cc42dac50108552ff1fc24d03d05a44c7f8f6476
mateus <mateus.oliveira@essentiagroup.com.br>
2024-09-11 16:40:22 -0300

    finalizando mais algumas estilizações para formulários e views

--- public/index.php
+++ public/index.php
@@ -5,7 +5,8 @@
  */
 chdir(dirname(__DIR__));
 ini_set('display_errors', true);
-error_reporting(E_ALL);
+
+// error_reporting(E_ALL|E_STRICT);
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
