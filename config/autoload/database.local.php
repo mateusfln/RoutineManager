2935de84d5c34c94e205f0ad0f20a35fe4c08817
Mateusfln <mateusfln@gmail.com>
2024-09-16 21:23:29 -0300

    .env

---
+++ config/autoload/database.local.php
@@ -0,0 +1,23 @@
+<?php
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'        => 'localhost',
+                    'port'        => '3306',
+                    'user'        => 'root',
+                    'password'    => 'Cookie20',
+                    'dbname'      => 'RoutineManager',
+                )
+            )
+        ),
+        'configuration' => array(
+            'orm_default' => array(
+                'proxy_dir' => 'core/server/data/DoctrineORMModule/Proxy',
+                'proxy_namespace' => 'DoctrineORMModule\Proxy',
+            )
+        )
+    ),
+);
