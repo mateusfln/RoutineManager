905dc5f2475055d9d09b241e00a4b67efdf4be5b
Mateusfln <mateusfln@gmail.com>
2024-09-16 21:28:38 -0300

    att config

--- config/autoload/database.local.php
+++ config/autoload/database.local.php
@@ -8,7 +8,7 @@ return array(
                     'host'        => 'localhost',
                     'port'        => '3306',
                     'user'        => 'root',
-                    'password'    => 'Cookie20',
+                    'password'    => '',
                     'dbname'      => 'RoutineManager',
                 )
             )
